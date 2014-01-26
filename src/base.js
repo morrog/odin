5b822e7a9efb88951bb7d995d8c8b63c04dee827
MartinForre <martin@martinforre.com>
2014-01-26 01:40:47 +0100

    The base class

---
+++ src/base.js
@@ -0,0 +1,175 @@
+var object = require("mout/object"),
+    lang = require("mout/lang"),
+    array = require("mout/array"),
+
+Base = function() {
+    var options = array.slice(arguments, -1);
+
+    if(lang.isObject(options[0])) {
+        this.setOptions(options[0]);
+    }
+
+    this._events = {};
+    return this.init.apply(this, arguments);
+};
+
+Base.prototype = {
+    constructor: Base,
+
+    _events: {},
+    mine: [],
+    defaults: {},
+    options: {},
+
+    init: function() {
+        return this;
+    },
+
+    setOptions: function(options) {
+        object.deepMixIn(this.options, this.defaults, options);
+        object.deepMixIn(this, object.pick(this.options, this.mine));
+
+        return this;
+    },
+
+    on: function(names, handler, context) {
+        var i, len, namespace, stack, ev;
+
+        names = lang.isArray(names) ? names : names.split(" ");
+
+        for(i = 0, len = names.length; i < len; i++) {
+            ev = this._createEvent(names[i], handler, context)
+            stack = this._getStack(ev.name);
+
+            stack.push(ev);
+        }
+
+        return this;
+    },
+
+    once: function(names, handler, context) {
+        var self = this, _handler = handler;
+
+        handler = function() { self.off(names, handler, context); _handler.apply(context, arguments); };
+
+        return this.on(names, handler, context);
+    },
+
+    off: function(names, handler, context) {
+        return this._eachEvent(names, function(e) {
+            var s = this._getStack(e.name); 
+            array.remove(s, e);
+            if(!s.length) delete this._events[e.name];
+        }, handler, context);
+    },
+
+    trigger: function(names) {
+        var args = array.slice(arguments, 1);
+
+        return this._eachEvent(names, function(e) {
+            this._triggerEvent(e, args);
+        });
+    },
+
+    _triggerEvent: function(ev, args) {
+        var context = ev.context || this;
+
+        if(!ev.handler) {
+            return;
+        }
+
+        switch(args.length) {
+            case 0:
+                ev.handler.call(context);
+                break;
+
+            case 1:
+                ev.handler.call(context, args[0]);
+                break;
+
+            case 2:
+                ev.handler.call(context, args[0], args[1]);
+                break;
+
+            case 3:
+                ev.handler.call(context, args[0], args[1], args[2]);
+                break;
+
+            case 4:
+                ev.handler.call(context, args[0], args[1], args[2], args[3]);
+                break;
+
+            default:
+                ev.handler.apply(context, args);
+        }
+    },
+
+    _eachEvent: function(names, callback, handler, context) {
+        var i, len, ev, stack, events;
+
+        names = lang.isArray(names) ? names : names.split(" ");
+
+        for(i = 0, len = names.length; i < len; i++) {
+            ev = this._createEvent(names[i], handler, context);
+            stack = this._getStack(ev.name);
+
+            if(!stack.length) {
+                continue;
+            }
+
+            events = array.filter(stack, ev);
+
+            if(!events || !events.length) {
+                continue;
+            }
+
+            array.forEach(events, function(e) {
+                if(e.namespace && e.namespace !== ev.namespace) return;
+                callback.call(this, e);
+            }, this);
+        }
+
+        return this;
+    },
+
+    _getStack: function(name) {
+        if(!name) return array.flatten(object.values(this._events));
+        return this._events[name] || (this._events[name] = []);
+    },
+
+    _createEvent: function(name, handler, context) {
+        var namespace, ev = { context: context };
+
+        name = name.split(":");
+        namespace = name[1];
+        name = name[0];
+
+        if(name) ev.name = name;
+        if(namespace) ev.namespace = namespace;
+        if(handler) ev.handler = handler;
+
+        return ev;
+    }
+};
+
+Base.extend = function(proto, static) {
+    var parent = this, constructor, derived;
+
+    proto = proto || {};
+
+    derived = object.hasOwn(proto, "constructor") ?
+                    proto.constructor :
+                    function() { parent.apply(this, arguments); };
+    
+    object.mixIn(derived, parent, static || {});
+    
+    lang.inheritPrototype(derived, parent);
+
+    object.mixIn(derived.prototype, proto);
+    
+    derived.prototype.constructor = derived;
+    
+    return derived;
+};
+
+module.exports = Base;
---
+++ test/base.js
@@ -0,0 +1,255 @@
+describe("The Base class", function() {
+    var Base = require("../src/base.js"),
+        array = require("mout/array");
+
+    it("Should be defined", function() {
+        expect(Base).toBeDefined();
+    });
+
+    it("Should call the init method upon creation", function() {
+        spyOn(Base.prototype, "init");
+
+        new Base;
+
+        expect(Base.prototype.init).toHaveBeenCalled();
+    });
+
+    it("Should pass arguments to the init method", function() {
+        var arg = "foo";
+
+        spyOn(Base.prototype, "init");
+
+        new Base(arg);
+
+        expect(Base.prototype.init).toHaveBeenCalledWith(arg);
+    });
+
+    it("Should assume the last argument as options and set .options", function() {
+        var options = { foo: "bar" },
+            base = new Base(null, "foo", options);
+
+        expect(base.options).toEqual(options);
+    });
+
+    it("Should map keys in the .mine array to properties on the instance from the options", function() {
+        var key = "____", value = "foo", base, options = {};
+
+        Base.prototype.mine = [key];
+        options[key] = value;
+
+        base = new Base(options);
+
+        expect(base[key]).toBe(value);
+    });
+
+    describe("Extending", function() {
+        it("Should have an extend method", function() {
+            expect(Base.extend).toBeDefined();
+        });
+
+        it("Should create a derived class", function() {
+            var Derived = Base.extend();
+
+            expect(new Derived instanceof Base).toBe(true);
+            expect(Derived.extend).toBe(Base.extend);
+        });
+
+        it("Should create a derived class with the provided prototype", function() {
+            var Derived = Base.extend({ foo: "bar" });
+
+            expect(Derived.prototype.foo).toBe("bar");
+        });
+
+        it("Should create a derived class with the provided static properties", function() {
+            var Derived = Base.extend({}, { foo: "bar" });
+
+            expect(Derived.foo).toBe("bar");
+        });
+
+        it("Should create a derived class with the provided constructor", function() {
+            var constructor = function() {},
+                Derived = Base.extend({ constructor: constructor }),
+                derived = new Derived;
+
+            expect(derived instanceof constructor).toBe(true);
+        });
+    });
+
+    describe("Events", function() {
+        it("Should clear the events stack upon creation", function() {
+            var base = new Base;
+
+            expect(base._events).not.toBe(Base.prototype._events);
+            expect(base._events).toEqual(jasmine.any(Object));
+        });
+
+        it("Should add simple events to the events stack", function() {
+            var base = new Base,
+                eventName = "foo", handler = function() {}, context = {},
+                stack;
+
+            base.on(eventName, handler, context);
+
+            stack = base._events[eventName];
+            expect(stack).toEqual(jasmine.any(Array));
+            expect(stack[0].handler).toBe(handler);
+            expect(stack[0].context).toBe(context);
+        });
+
+        it("Should add a string of events to the events stack", function() {
+            var base = new Base,
+                events = "foo bar", handler = function() {}, context = {};
+
+            base.on(events, handler, context);
+
+            array.forEach(events.split(" "), function(name) {
+                var stack = base._events[name];
+
+                expect(stack[0].handler).toBe(handler);
+                expect(stack[0].context).toBe(context);
+            });
+        });
+
+        it("Should add an array of events to the events stack", function() {
+            var base = new Base,
+                events = ["foo", "bar"], handler = function() {}, context = {};
+
+            base.on(events, handler, context);
+
+            array.forEach(events, function(name) {
+                var stack = base._events[name];
+
+                expect(stack[0].handler).toBe(handler);
+                expect(stack[0].context).toBe(context);
+            });
+        });
+
+        it("Should add namespaced events to the events stack", function() {
+            var base = new Base,
+                eventName = "foo", namespace = "bar", handler = function() {}, context = {},
+                stack;
+
+            base.on(eventName + ":" + namespace, handler, context);
+
+            stack = base._events[eventName];
+            expect(stack).toEqual(jasmine.any(Array));
+            expect(stack[0].handler).toBe(handler);
+            expect(stack[0].context).toBe(context);
+            expect(stack[0].namespace).toBe(namespace);
+        });
+
+        it("Should remove events by name", function() {
+            var base = new Base,
+                eventName = "foo";
+
+            base.on(eventName)
+                .on(eventName + ":namespace");
+            base.off(eventName);
+
+            expect(base._events[eventName].length).toBe(1);
+        });
+
+        it("Should remove events by namespace", function() {
+            var base = new Base,
+                eventName = "foo", namespace = "bar",
+                stack;
+
+            base.on(eventName)
+                .on(eventName + ":" + namespace);
+
+            base.off(":" + namespace);
+
+            stack = base._events[eventName];
+
+            expect(stack.length).toBe(1);
+            expect(stack[0].namespace).not.toBe(namespace);
+        });
+
+        it("Should remove events by handler", function() {
+            var base = new Base,
+                eventName = "foo", handler = function() {},
+                stack;
+
+            base.on(eventName, handler)
+                .on(eventName, function() {});
+
+            base.off(eventName, handler);
+
+            stack = base._events[eventName];
+
+            expect(stack.length).toBe(1);
+            expect(stack[0].handler).not.toBe(handler);
+        });
+
+        it("Should remove events by handler and context", function() {
+            var base = new Base,
+                eventName = "foo", handler = function() {}, context = {},
+                stack;
+
+            base.on(eventName, handler, context)
+                .on(eventName, handler)
+                .on(eventName, function() {});
+
+            base.off(eventName, handler, context);
+
+            stack = base._events[eventName];
+            expect(stack.length).toBe(2);
+        });
+
+        it("Should trigger a simple event", function() {
+            var base = new Base,
+                eventName = "foo", handler = jasmine.createSpy("handler");
+
+            base.on(eventName, handler)
+                .trigger(eventName);
+
+            expect(handler).toHaveBeenCalled();
+        });
+
+        it("Should trigger a namespaced event", function() {
+            var base = new Base,
+                eventName = "foo", namespace = ":bar", handler = jasmine.createSpy("handler"), notCalledHandler = jasmine.createSpy("handler");;
+
+            base.on(eventName + namespace, handler)
+                .on(eventName, notCalledHandler)
+                .trigger(namespace);
+
+            expect(handler).toHaveBeenCalled();
+            expect(notCalledHandler).not.toHaveBeenCalled();
+        });
+
+        it("Should pass arguments to a handler", function() {
+            var base = new Base,
+                eventName = "foo", arg = "bar", handler = jasmine.createSpy("handler");
+
+            base.on(eventName, handler)
+                .trigger(eventName, arg);
+
+            expect(handler).toHaveBeenCalledWith(arg);
+        });
+
+        it("Should only trigger handlers added with .once() one time", function() {
+            var base = new Base,
+                eventName = "foo", handler = jasmine.createSpy("handler");
+
+            base.once(eventName, handler)
+                .trigger(eventName)
+                .trigger(eventName)
+                .trigger(eventName)
+                .trigger(eventName);
+
+            expect(handler.calls.length).toBe(1);
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
