7c80b33195c158e06700100f1b6a12e53605fa10
Anders Ladegaard Marchsteiner <KGHSPEDITION\andersm>
2014-02-11 14:26:41 +0100

    Add test for globals being setted in browsers.

--- src/odin.js
+++ src/odin.js
@@ -3,9 +3,32 @@ var Base = require("./base"),
     Model = require("./model"),
     Validator = require("./validator"),
     Storage = require("./storage"),
-    LocalStorage = require("./localStorage");
+    LocalStorage = require("./localStorage"),
+    defined = false;
 
 module.exports = {
+    defineMe: function(isBrowser) {
+        if(defined && !process.env.TEST) {
+            return;
+        }
+
+        defined = true;
+
+        // Expose Odin as an amd module
+        Base.defineMe.call(module.exports);
+
+        if(isBrowser) {
+            window.Odin = module.exports;
+
+            Injector.static("window", window);
+            Injector.static("window.localStorage", window.localStorage);
+
+            Injector.static("$", $);
+            Injector.static("ajax", $.ajax);
+        } else {
+            // Require REST service and other node.js schtuff here
+        }
+    },
     Base: Base,
     Injector: Injector,
     Model: Model,
@@ -14,18 +37,4 @@ module.exports = {
     LocalStorage: LocalStorage
 };
 
-if(process.browser) {
-    window.Odin = module.exports;
-
-    
-    Injector.static("window", window);
-    Injector.static("window.localStorage", window.localStorage);
-    
-    Injector.static("$", $);
-    Injector.static("ajax", $.ajax);
-} else {
-	// Require REST service and other node.js schtuff here
-}
-
-// Expose Odin as an amd module
-Base.defineMe.call(module.exports);
+module.exports.defineMe(process.browser !== undefined);
